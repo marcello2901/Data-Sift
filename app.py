a97b3e5549a42b81307eac4d459ff4efdd03784a
marcello2901 <marcello613@gmail.com>
2026-05-19 17:20:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1031,8 +1031,8 @@ def main():
                     show_trendlines = False
 
                     if chart_type in ['Moving Average', 'Moving Median']:
-                        show_trendlines = gc3.checkbox("Plateau Lines", value=True, help="Draws horizontal lines based on Harris-Boyd cuts.")
-                        gc3.markdown(f"<div style='margin-top: 5px;'>{help_icon}</div>", unsafe_allow_html=True)
+                        show_trendlines = gc3.checkbox("Plateau Lines", value=True)
+                        gc3.markdown(f"<div style='margin-top: 5px; <span title='Draws horizontal lines based on Harris-Boyd cuts'>{help_icon}</span></div>", unsafe_allow_html=True)
                         
                     if st.session_state.col_sexo and st.session_state.sex_column_is_valid:
                         group_by_sex_plot = gc4.checkbox("Group by Sex", value=False)
